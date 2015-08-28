8d16bf9c354277ca167d225de2a38adc82767751
hasankryeziusrf <hasan.kryeziu@srf.ch>
2015-08-28 13:39:25 +0200

    Commit the EzLegacyProfilerBundle

---
+++ EzLegacyProfilerBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EzLegacyProfilerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzLegacyProfilerBundle extends Bundle
+{
+     protected $name = 'EzLegacyProfilerBundle';
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+Install Instructions
+====================
+
+* Register the Bundle in the `EzPublishKernel`
+* Set in the site.ini under [DebugSettings] `Debug=only_in_symfony`
+
+
